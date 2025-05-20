0b473daae86ac7e501f7bb8cf9cc5dc9e48f876b
NarayanaGolla <narayana.golla.aws@gmail.com>
2025-05-20 08:38:42 +0530

    print samples

--- basic/printsample/printSatementExamples.py
+++ basic/printsample/printSatementExamples.py
@@ -1,2 +1,21 @@
 # Example-1
 print("shiva")
+
+#Formatting with placeholders
+print("I'm going to inject %s here." %'Shiva')
+print("I'm going to inject %s text here, and %s text here.%s" %('some','more','shiva'))
+
+x, y , z= 'some', 'more' , 'shiva'
+print("I'm going to inject %s text here, and %s text here. %s"%(x,y,z))
+
+
+#Format conversion methods.
+print('He said his name was %s.' %'Fred')
+print('He said his name was %r.' %'<Fred')
+
+
+print('I once caught a fish %s.' %'this \tbig')
+print('I once caught a fish %r.' %'this \tbig')
+
+print('I wrote %s programs today.' %3.75)
+print('I wrote %d programs today.' %3.75)
