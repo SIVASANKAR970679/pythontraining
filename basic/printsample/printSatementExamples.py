99d2ce2f81d1c7b9486cfec1dd62139127dc4c2e
NarayanaGolla <narayana.golla.aws@gmail.com>
2025-05-21 06:45:31 +0530

    print samples

--- basic/printsample/printSatementExamples.py
+++ basic/printsample/printSatementExamples.py
@@ -19,3 +19,47 @@ print('I once caught a fish %r.' %'this \tbig')
 
 print('I wrote %s programs today.' %3.75)
 print('I wrote %d programs today.' %3.75)
+
+#Padding and Precision of Floating Point Numbers
+print('Floating point numbers: %5.2f' %(13.144))
+print('Floating point numbers: %1.0f' %(13.144))
+print('Floating point numbers: %1.5f' %(13.144))
+print('Floating point numbers: %10.2f' %(13.144))
+print('Floating point numbers: %25.2f' %(13.144))
+
+#Multiple Formatting
+print('First: %s, Second: %5.2f, Third: %r' %('hi!',3.1415,'bye!'))
+
+# Formatting with the .format() method
+print('This is a string with an {}'.format('insert'))
+print('The {2} {1} {0}'.format('fox','brown','quick'))
+print('First Object: {a}, Second Object: {b}, Third Object: {c}'.format(a=1,b='Two',c=12.3))
+print('A %s saved is a %s earned.' %('penny','penny'))
+# vs.
+print('A {p} saved is a {p} earned.'.format(p='penny'))
+
+#Formatted String Literals (f-strings)
+name = 'Tharun'
+print(f"He said his name is {name}.")
+print(f"He said his name is {name!r}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
